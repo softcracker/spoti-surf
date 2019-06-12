b2da8df315355a9ee905142275e40ecfe0c1271b
Marlena Flüh <marlena.flueh@white-paper-media.de>
2019-06-12 09:38:43 +0200

    fix port

--- server.js
+++ server.js
@@ -1,24 +1,22 @@
-const fs = require('fs')
 const http = require('http')
-const proxy = require('http-proxy')
+const httpProxy = require('http-proxy')
+const PORT = 8000
+const PORT2 = 9000
 
-const PORT = process.env.PORT || 9000
-const SecondPORT = process.env.PORT || 443
+// Create your proxy server and set the target in the options.
+try {
+    httpProxy.createProxyServer({target:'http://localhost:9000'}).listen(PORT)
+} catch (err) {
+    console.log(err)
+}
 
-proxyServer = proxy.createProxyServer({target:'https://spoti-surf.heroku.com'})
-
-proxyServer.listen(80)
-
-server = http.createServer((req, res) => {
-    console.log("http request")
-    proxyServer.web(req, res, {
-        target: req.url
-    })
-
-    proxyServer.on('error', function(e) {
-        console.log("Error in proxy call")
-    })
-    proxyServer.listen(SecondPORT)
-})
-
-server.listen(PORT)
+// Create your target server
+try {
+    http.createServer(function (req, res) {
+      res.writeHead(200, { 'Content-Type': 'text/plain' })
+      res.write('request successfully proxied!' + '\n' + JSON.stringify(req.headers, true, 2))
+      res.end()
+    }).listen(PORT2)
+} catch (err) {
+    console.log(err)
+}
