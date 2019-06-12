d3d62bd4d3065c93e14e5e5da34db910a73dbec1
Marlena Flüh <marlena.flueh@white-paper-media.de>
2019-06-12 10:30:32 +0200

    add dynamic url

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ try {
 // Create your target server
 try {
     http.createServer(function (req, res) {
-      proxy.web(req, res, {target: "https://spotify.com"})
+      proxy.web(req, res, {target: req.url})
     }).listen(PORT2)
 } catch (err) {
     console.log(err)
